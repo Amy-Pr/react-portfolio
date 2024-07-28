207ebd35889ff76e4c31bccf07104d5846d674c0
amy_pr <ampreci@gmail.com>
2024-07-28 16:25:38 -0700

    move hero section in App component, style hero section for desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ const App = () => {
   return (
     <div>
       <Header />
+      <Hero />
       <main>
-        <Hero />
+        {/* <Hero /> */}
         <About />
         <Projects />
         <Contact />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ import Link from './Link';
 const Hero = () => {
     return(
         <section className="hero divider">
-            <div className="image">
+            {/* <div className="image">
                 <img src={profileImage} alt="Photo of Amy"/>
-            </div>
-            <div>
-                <h1>Amy Preci</h1>
-                <p>Web Developer</p>
+            </div> */}
+            <div className="intro">
+                <h1>Hello, I'm Amy Preci</h1>
+                <p>I am a Web Developer in</p>
                 <p>Portland, Oregon</p>
                 <Link href="#projects">Check out my work</Link>
             </div>
