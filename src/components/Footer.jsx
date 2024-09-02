dfbcf59d39a4c165e1b1f0f2a80892c8f0f3d66e
amy_pr <ampreci@gmail.com>
2024-09-02 00:31:22 -0700

    add icon to browser tab and footer, change footer spacing on media query

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
+import greyhound from "../assets/greyhound_icon_one_hundred.png";
+
 const Footer = () => {
     const currentYear = new Date().getFullYear()
     return (
         <footer>
             <span>Amy Preci {currentYear}&copy;</span>
+            <img src={greyhound}/>
         </footer>
     );
 };
